a771c2622acbe0445fa563b25dd9d58941580221
Eryn Wells <eryn@erynwells.me>
2018-11-03 15:15:03 -0400

    Create Terrain2 app, using Xcode's boilerplate

---
+++ Terrain2/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Terrain2
+//
+//  Created by Eryn Wells on 11/3/18.
+//  Copyright © 2018 Eryn Wells. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
