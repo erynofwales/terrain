55f7c6fe7856494a40af19a998ed2f9940011471
Eryn Wells <eryn@erynwells.me>
2018-11-20 11:00:59 -0700

    Add Math module and an extension to float4 to make a float3

---
+++ Terrain2/Math.swift
@@ -0,0 +1,16 @@
+//
+//  Math.swift
+//  Terrain2
+//
+//  Created by Eryn Wells on 11/20/18.
+//  Copyright © 2018 Eryn Wells. All rights reserved.
+//
+
+import Foundation
+import simd
+
+extension float4 {
+    var xyz: float3 {
+        return float3(x, y, z)
+    }
+}
