4e848091bb1d067c16522ee1e5857e3158d99607
Eryn Wells <eryn@erynwells.me>
2018-11-23 09:20:18 -0700

    Soften the Diamond-Square roughness

--- Terrain2/Terrain.swift
+++ Terrain2/Terrain.swift
@@ -109,7 +109,7 @@ class Terrain: NSObject {
             print("Couldn't create algorithm")
             return nil
         }
-        gen.roughness = 0.075
+        gen.roughness = 0.05
         generator = gen
 
         do {
